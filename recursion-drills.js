68d247478c13e1c2846fc3abc132870771a04984
Matt Williamson <mathewbwilliamson@gmail.com>
2018-11-05 12:17:10 -0500

    Finished Reverse String

--- recursion-drills.js
+++ recursion-drills.js
@@ -1,19 +1,31 @@
-function counter(sheep) {
-  if (sheep === 0) {
+'use strict';
+
+// function counter(sheep) {
+//   if (sheep === 0) {
+//     return '';
+//   }
+
+//   console.log(sheep + ' - Another sheep jump over the fence');
+//   return counter(sheep - 1)
+// }
+
+// // console.log(counter(3));
+
+// function doubleArray(arr) {
+//   if(arr.length === 0) {
+//     return '';
+//   }
+//   return [arr[0] * 2, ...doubleArray(arr.slice(1))]
+// }
+
+// console.log(doubleArray([1,2,3]))
+
+function reverseString(myStr) {
+  if (myStr.length === 0) {
     return '';
   }
-
-  console.log(sheep + ' - Another sheep jump over the fence');
-  return counter(sheep - 1)
+  
+  return myStr[myStr.length-1] + reverseString(myStr.slice(0, myStr.length-1));
 }
 
-console.log(counter(3));
-
-function doubleArray(arr) {
-  if(arr.length === 0) {
-    return '';
-  }
-  return [arr[0] * 2, ...doubleArray(arr.slice(1))]
-}
-
-console.log(doubleArray([1,2,3]))
+console.log(reverseString('This is a string'))
